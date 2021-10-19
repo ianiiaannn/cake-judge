5331adcc68a16a2d2314e7a47e0e2e888ed1dda3
ianiiaannn <ianiiaannn@gmail.com>
2021-10-19 23:22:22 +0800

    db(1/5)?

--- app.mjs
+++ app.mjs
@@ -2,13 +2,18 @@ const PORT = 8080;
 
 import express from 'express';
 import http from 'http';
-
+import mariadb from 'mariadb';
 
 const app=express();
 http.createServer(app);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+const pool=mariadb.createPool({
+  host: '127.0.0.1',
+});
 
 app.get('/', (req, res)=>{
   res.render('index');
